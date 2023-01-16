c869fd86478b707576fe9401330bf2fa97fce383
colemaring <65455664+colemaring@users.noreply.github.com>
2023-01-15 19:55:41 -0500

    Added functionality to the toggle settings

--- main.js
+++ main.js
@@ -1,21 +1,86 @@
-function generate() {
-    let str = "";
-    let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let lowercase = "abcdefghijklmnopqrstuvwxyz";
+let numbers = "0123456789";
+let symbols = "";
+let custom = "";
 
-    for(let i = 0; i < document.getElementById("lengthslider").value; i++) {
+function generate() 
+{
+    let characters = uppercase + lowercase + numbers + symbols;
+    let str = "";
+
+    for (let i = 0; i < document.getElementById("lengthslider").value; i++) 
+    {
         str += characters.charAt(Math.floor(Math.random() * characters.length));
     }
 
     document.getElementById("output").value = str;
 }
 
-function settings() {
+// toggles the "Advanced Settings" menu by checking the style state of the button
+function settings() 
+{
     let selector = document.getElementById("settings");
 
-    if(selector.style.display === "none"){
+    if (selector.style.display === "none")
+    {
         selector.style.display = "block";
     }
-    else {
+
+    else 
+    {
         selector.style.display = "none"
     }
+}
+
+// toggles the inclusion of numbers in the password
+function toggleNumbers(cb)
+{
+    numbers = "";
+
+    if (cb.checked)
+    {
+        numbers = "0123456789";
+    }
+}
+
+// toggles the inclusion of uppercase characters in the password
+function toggleUppercase(cb)
+{
+    uppercase = "";
+
+    if (cb.checked)
+    {
+        uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+}
+
+// toggles the inclusion of lowercase characters in the password
+function toggleLowercase(cb)
+{
+    lowercase = "";
+
+    if (cb.checked)
+    {
+        lowercase = "abcdefghijklmnopqrstuvwxyz";
+    }
+}
+
+// toggles the inclusion of symbols in the password
+function toggleSymbols(cb)
+{
+    symbols = "";
+
+    if (cb.checked)
+    {
+        symbols = "!@#$%&";
+    }
+}
+
+// copies the generated output to the clipboard
+function copy()
+{
+    let copyText = document.getElementById("output").value;
+    
+    navigator.clipboard.writeText(copyText);
 }
